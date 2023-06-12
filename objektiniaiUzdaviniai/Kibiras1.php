4ff0dded3b0415832d93aa7d849be8f663d20aac
sauliuskarlinskas <127085630+sauliuskarlinskas@users.noreply.github.com>
2023-06-12 21:59:53 +0300

    nd 06.12

---
+++ objektiniaiUzdaviniai/Kibiras1.php
@@ -0,0 +1,11 @@
+<?php
+
+class Kibiras1
+{
+    private $akmenuKiekis = 0;
+
+    public function prideti1Akmeni() : string
+    {
+        return $this->prideti1Akmeni() + 1;
+    }
+}
---
+++ objektiniaiUzdaviniai/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/Kibiras1.php';
+
+echo 'Kibiras1';
